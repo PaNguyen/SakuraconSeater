7dbcd8f0776de555d05ba9a4a9f1709aa24e984f
Blaise Ritchie <blaise@blaise.in>
2018-03-27 18:09:15 -0700

    Added default mysettings

---
+++ mysettings.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+##########################
+#  CUSTOM USER SETTINGS  #
+##########################
+#  UNCOMMENT ANY OF THE  #
+#   FOLLOWING LINES TO   #
+#   CONFIGURE THE APP.   #
+##########################
+#  THE APP WILL USE THE  #
+#  DEFAULTS IF YOU DONT  #
+##########################
+
+
+# DB
+#  DBFILE is the name of the file that contains the scores and player database.
+#DBFILE = "tables.db"
+#DBBACKUPS = "backups"
+#DBDATEFORMAT = "%Y-%m-%d-%H-%M-%S"
+#MEMCACHE = ""
+
+# PASSWORD is the admin password for managing your instance
+#PASSWORD='$pbkdf2-sha256$29000$xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+# TWILIO settings for the Twilio text API
+#TWILIO_SID="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#TWILIO_AUTH="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+# default GAME_DURATION
+#GAME_DURATION=60 * 60
