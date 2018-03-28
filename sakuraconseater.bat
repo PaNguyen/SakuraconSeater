2772fcb559e3d77da7b59cf722115b9f74b28aee
Blaise Ritchie <blaise@blaise.in>
2018-03-27 17:27:20 -0700

    Added support for 32 bit windows

--- sakuraconseater.bat
+++ sakuraconseater.bat
@@ -1,5 +1,7 @@
 @echo off
-set path=%path%;%PROGRAMFILES(X86)%\SakuraconSeater;%PROGRAMFILES(X86)%\SakuraconSeater\lib;%USERPROFILE%\Documents\SakuraconSeater
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32bit || set OS=64bit
+if %OS%==32bit set path=%path%;%PROGRAMFILES%\SakuraconSeater;%PROGRAMFILES%\SakuraconSeater\lib;%USERPROFILE%\Documents\SakuraconSeater
+if %OS%==64bit set path=%path%;%PROGRAMFILES(X86)%\SakuraconSeater;%PROGRAMFILES(x86)%\SakuraconSeater\lib;%USERPROFILE%\Documents\SakuraconSeater
 start sakuraconseater.exe
 timeout /t 3
 start "" http://localhost:5000
