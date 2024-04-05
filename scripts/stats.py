5d8e1293fc6103098d3a8481fcea65e317083fc9
Patrick Nguyen <pahdnguyen@gmail.com>
2024-04-05 14:41:28 -0700

    Simplify and fix simulation

--- scripts/stats.py
+++ scripts/stats.py
@@ -15,60 +15,67 @@ args = parser.parse_args()
 con = sqlite3.connect(args.db_filename)
 cur = con.cursor()
 
-session = 0
 tables: dict[int, dict[str, any]] = {}
-players: dict[str, dict[str, any]] = {} # players ID'ed by session and id within a session
+players: dict[int, dict[str, any]] = {}
 table_stats = []
+player_stats = []
 for (event_type, time, data) in cur.execute("SELECT * FROM Events ORDER BY time ASC;"):
     print(event_type, time, data)
     if event_type == 'start':
-        session = session + 1
         pass
     elif event_type == 'tablecreate':
         table_id = int(json.loads(data))
         if table_id in tables:
             print(f"WARNING overwriting table {table_id}")
-        tables[table_id] = {'id': table_id, 'time_created': time, 'players': []}
+        tables[table_id] = {'id': table_id, 'time_created': time, 'table_type': "default", 'players': []}
         pass
     elif event_type == 'tabledelete':
         print(f"WARNING tabledelete({table_id}) encountered")
         table_id = int(json.loads(data))
         del tables[table_id]
         pass
+    elif event_type == 'tableretype':
+        table_id, table_type = json.loads(data)
+        tables[int(table_id)]['table_type'] = table_type
     elif event_type == 'playerqueueadd':
-        id, name, table_type, num_players = json.loads(data)
-        player_id = f"{session}_{id}"
+        player_id, name, table_type, num_players = json.loads(data)
         if player_id in players:
-            print(f"ERROR duplicate player {player_id}")
+            print(f"WARNING overwriting player {player_id}")
         # TODO is num_players worth tracking since players are also split into separate entries?
         players[player_id] = {'id': player_id, 'name': name, 'table_type': table_type, 'num_players': num_players, 'time_added_to_queue': time}
         pass
     elif event_type == 'playermovetotable':
-        id, table_id = json.loads(data)
-        player_id = f"{session}_{id}"
+        player_id, table_id = json.loads(data)
+        player_id = int(player_id)
         table_id = int(table_id)
         tables[table_id]['players'].append(player_id)
         players[player_id]['time_added_to_table'] = time
+        players[player_id]['table_added_to'] = table_id
         pass
     elif event_type == 'tablestart':
         table_id = int(json.loads(data))
         tables[table_id]['time_start'] = time
+        for player_id in tables[table_id]['players']:
+            players[player_id]['table_start_time'] = time
         pass
     elif event_type == 'tableclear':
         table_id = int(json.loads(data))
         table = tables[table_id]
         table['time_end'] = time
+        for player_id in table['players']:
+            players[player_id]['table_clear_time'] = time
         table_stats.append(tables[table_id].copy())
+        for player_id in table['players']:
+            player_stats.append(players[player_id].copy())
         table['players'] = []
         del table['time_start']
         del table['time_end']
         pass
     elif event_type == 'playerdelete':
-        id = json.loads(data)
-        player_id = f"{session}_{id}"
-        print(str(players))
+        player_id = int(json.loads(data))
         if player_id in players:
             players[player_id]['time_deleted'] = time
+            player_stats.append(players[player_id].copy())
         pass
     elif event_type == 'tablefill':
         # TODO what is this?
@@ -82,4 +89,4 @@ for (event_type, time, data) in cur.execute("SELECT * FROM Events ORDER BY time 
         print(f"WARNING Unknown event {event_type}")
 
 print(json.dumps(table_stats))
-print(json.dumps(players))
+print(json.dumps(player_stats))
